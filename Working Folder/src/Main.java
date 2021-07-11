0cc0940ae382eddf5389d6f2e4ef742a6e4d3832
MullaiArgent <mullairajan2000@gmail.com>
2021-07-11 18:17:51 +0530

    Threads extended and MouseListener Implemented, to the all Events. Images for all the Instance are filed and paths are targeted.

--- Working Folder/src/Main.java
+++ Working Folder/src/Main.java
@@ -1,10 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
 public class Main{
     public static void main(String[] args) {
-        WorkingClass start = new WorkingClass();
+        new WorkingClass();
     }
 }
--- Working Folder/src/WorkingClass.java
+++ Working Folder/src/WorkingClass.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class WorkingClass implements MouseListener {
+public class WorkingClass{
     JLabel l1 = new JLabel();
     JLabel l2 = new JLabel();
     JLabel l3 = new JLabel();
@@ -13,6 +12,7 @@ public class WorkingClass implements MouseListener {
     JLabel l7 = new JLabel();
     JLabel l8 = new JLabel();
     JLabel l9 = new JLabel();
+    JLabel alll = new JLabel();
 
     ImageIcon i1  = new ImageIcon("pics\\1.jpg");
     ImageIcon i2  = new ImageIcon("pics\\2.jpg");
@@ -23,43 +23,26 @@ public class WorkingClass implements MouseListener {
     ImageIcon i7  = new ImageIcon("pics\\7.jpg");
     ImageIcon i8  = new ImageIcon("pics\\8.jpg");
     ImageIcon i9  = new ImageIcon("pics\\9.jpg");
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Watching Tom");
     WorkingClass(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600,600);
 
         l1.setBounds(0,0,200,200);
-        l1.setBackground(Color.cyan);
-        l1.setOpaque(true);
-        l1.addMouseListener(this);
-
         l2.setBounds(200,0,200,200);
-        l2.addMouseListener(this);
-
         l3.setBounds(400,0,200,200);
-        l3.addMouseListener(this);
-
         l4.setBounds(0,200,200,200);
-        l4.addMouseListener(this);
-
         l5.setBounds(200,200,200,200);
-        l5.addMouseListener(this);
-
+        l2.setText("Hover the Cursor");
         l6.setBounds(400,200,200,200);
-        l6.addMouseListener(this);
-
         l7.setBounds(0,400,200,200);
-        l7.addMouseListener(this);
-
         l8.setBounds(200,400,200,200);
-        l8.addMouseListener(this);
-
         l9.setBounds(400,400,200,200);
-        l9.addMouseListener(this);
-
+        alll.setBounds(0,0,600,600);
 
         frame.setLayout(null);
         frame.setVisible(true);
+
         frame.add(l1);
         frame.add(l2);
         frame.add(l3);
@@ -69,31 +52,244 @@ public class WorkingClass implements MouseListener {
         frame.add(l7);
         frame.add(l8);
         frame.add(l9);
+        frame.add(alll);
 
+        one one = new one();
+        one.start();
+        two two = new two();
+        two.start();
+        three three = new three();
+        three.start();
+        four four = new four();
+        four.start();
+        five five = new five();
+        five.start();
+        six six = new six();
+        six.start();
+        seven seven = new seven();
+        seven.start();
+        eight eight = new eight();
+        eight.start();
+        nine nine = new nine();
+        nine.start();
 
     }
+
+class one extends Thread implements MouseListener{
+        one(){
+            l1.addMouseListener(this);
+        }
     @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
+    public void run(){    }
 
     @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
+    public void mouseClicked(MouseEvent e) {    }
 
     @Override
-    public void mouseReleased(MouseEvent e) {
+    public void mousePressed(MouseEvent e) {    }
 
-    }
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        System.out.print("YEah");
+        alll.setIcon(i1);
     }
 
     @Override
-    public void mouseExited(MouseEvent e) {
+    public void mouseExited(MouseEvent e) {    }
+}
+class two extends Thread implements MouseListener{
+        two(){
+            l2.addMouseListener(this);
+        }
+    @Override
+    public void run(){    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i2);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
+class three extends Thread implements MouseListener{
+        three(){
+            l3.addMouseListener(this);
+        }
+    @Override
+    public void run(){    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i3);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
+class four extends Thread implements MouseListener{
+        four(){
+            l4.addMouseListener(this);
+        }
+    @Override
+    public void run(){    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i4);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
+class five extends Thread implements MouseListener{
+        five(){
+            l5.addMouseListener(this);
+        }
+    @Override
+    public void run(){    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i5);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
+class six extends Thread implements MouseListener{
+        six(){
+            l6.addMouseListener(this);
+        }
+    @Override
+    public void run(){    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i6);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
+class seven extends Thread implements MouseListener{
+        seven(){
+            l7.addMouseListener(this);
+        }
+    @Override
+    public void run(){    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i7);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
+class eight extends Thread implements MouseListener{
+        eight(){
+            l8.addMouseListener(this);
+        }
+    @Override
+    public void run(){    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i8);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}
+class nine extends Thread implements MouseListener{
+        nine(){
+            l9.addMouseListener(this);
+        }
+    @Override
+    public void run(){
 
     }
-}
+
+    @Override
+    public void mouseClicked(MouseEvent e) {    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {   }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        alll.setIcon(i9);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {    }
+}}
